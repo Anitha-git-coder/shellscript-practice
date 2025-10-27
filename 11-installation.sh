dbd26783d11af15e587176ee6a3a58f4f6505901
Anitha <anitha.pinipe14@gmail.com>
2025-10-27 10:51:05 +0530

    variables 24-10-25

--- 11-installation.sh
+++ 11-installation.sh
@@ -5,3 +5,11 @@ USERID=$(id -u)
 if [ $USERID  -ne 0 ]; then
     echo "error :: run with root privelege"
 fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "error:: while installing"
+ else 
+    echo "installing mysql is success"
+fi
