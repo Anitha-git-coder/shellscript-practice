f6bb8afca3b05163e03dd05de75bdd2d65872b0d
Anitha <anitha.pinipe14@gmail.com>
2025-11-06 14:30:48 +0530

    while loop

--- disk-usage.sh
+++ disk-usage.sh
@@ -2,7 +2,7 @@
 DISK_USAGE=$(df -hT | grep -v filesystem)
 DISK_THRESHOLD=2 # in projects its 75 
 IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
-MESSAGE=
+MESSAGE=""
 
 while IFS= read  -r line 
 do 
