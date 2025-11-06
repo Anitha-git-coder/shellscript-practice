61c72c6774b7e462eada5052c0c11f22366eb935
Anitha <anitha.pinipe14@gmail.com>
2025-11-06 14:27:47 +0530

    while loop

--- disk-usage.sh
+++ disk-usage.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 DISK_USAGE=$(df -hT | grep -v filesystem)
 DISK_THRESHOLD=2 # in projects its 75 
+IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+MESSAGE=
 
 while IFS= read  -r line 
 do 
 USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
 PARTITION=$(echo $line | awk '{print $7}') 
 if [ $USAGE -ge $DISK_THRESHOLD ]; then 
-echo "high usage on $PARTITION: $USAGE"
+# echo "high usage on $PARTITION: $USAGE"
+MESSAGE="high usage on $PARTITION: $USAGE %"
 fi
 done <<< $DISK_USAGE
+
+echo "message body : $MESSAGE"
